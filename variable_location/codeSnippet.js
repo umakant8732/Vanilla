0fe946a4642ea0cf85256b5e2dd07f7559016668
uma123 <umakantbhendarkar94@gmail.com>
2024-10-26 12:21:44 +0530

    made variable location

---
+++ variable_location/codeSnippet.js
@@ -0,0 +1,12 @@
+let username = "uma123"
+let nickname = "umakant"
+let age = 45 ;
+let mob = 958841 ;
+let isAdult = true;  
+let isMarried = false;
+
+
+
+
+// boolean, null, undefined values having same address in every website because they have similar values
+//num, string have different address coz they have different values but if same values have same address in memory visulization
